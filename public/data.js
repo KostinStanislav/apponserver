37ac5901fe6cf0c3f544fdfc5e3bcec920facbe8
KostinStanislav <tigrarion@mail.ru>
2016-12-06 22:45:18 +0000

    cannot CSS include

---
+++ public/data.js
@@ -0,0 +1,22 @@
+[   {
+    "id":"1",
+    "email":"tigra.mail.ru",
+    "firstname":"Stas",
+    "lastname":"Kostin"
+},{
+    "id":"2",
+    "email":"panda.mail.ru",
+    "firstname":"Olena",
+    "lastname":"Kravetz"
+},{
+    "id":"3",
+    "email":"fox.mail.ru",
+    "firstname":"Volodumur",
+    "lastname":"Zelenskij"
+},{
+    "id":"4",
+    "email":"hipo.mail.ru",
+    "firstname":"John",
+    "lastname":"Doe"
+}
+]
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
-app.use(express.static('public'));
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.set('view engine', 'jade');
+app.use('/static', express.static('public'));
+var dbFile = fs.readFileSync('./public/data.js');
+var ourData = JSON.parse(dbFile);
+app.get('/', function(req, res){
+    res.render('tempe', {title: "CRUD APP", message: "Table Data 'USERS'", db: ourData});
 });
-app.listen(process.env.PORT);
-console.log('Server has started!');
+app.listen(process.env.PORT, function(){
+    console.log('We are listening some port');
+});
